a00d44b5e1cfb8822f75df837e030faba876c39d
Jie Li <jiel@stud.ntnu.no>
2015-11-10 18:02:39 +0100

    Add popup.js

---
+++ LCH/js/popup.js
@@ -0,0 +1,108 @@
+window.addEventListener("load", function() {
+	//Package id's for popup
+	document.getElementById("pack-1").addEventListener("click", function() {popUp(1);});
+	document.getElementById("pack-2").addEventListener("click", function() {popUp(2);});
+	//Redirects to order.html
+	document.getElementById("order").addEventListener("click", function() {closePage();});
+	//Close window
+	document.getElementById("close").addEventListener("click", function() {closePage();});
+
+});
+
+
+function popUp(id){
+	var subWindow = window.open(packages/subpackages.html,"Subpackage",
+                                   "toolbar = no, location = no, status = no, menubar = no, scrollbars = yes, resizable = no, width = 800px", false);
+
+	if (id === 1){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 2){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 3){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 4){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 5){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 6){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 7){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 8){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	}
+	else if (id === 9){
+		subWindow.getElementById("package-header").innerHTML = "";
+		subWindow.getElementById("p1").innerHTML = "";
+		subWindow.getElementById("p2").innerHTML = "";
+		subWindow.getElementById("p3").innerHTML = "";
+		subWindow["img1"].src = "";
+		subWindow["img2"].src = "";
+		subWindow["img3"].src = "";
+	} 
+	
+}
+
+function redirectOrder(id) {
+	closePage();
+	window.location = "order.html";
+}
+
+function closePage(){
+	window.close();
+}
